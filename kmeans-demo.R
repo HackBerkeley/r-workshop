db34a98dbcecf5e5b967cced65443b0a5b30264d
nurakawa <nkawa@berkeley.edu>
2017-03-08 10:05:12 -0800

    modified for mnist data

--- kmeans-demo.R
+++ kmeans-demo.R
@@ -8,7 +8,6 @@
 #           iris dataset
 # =============================================================================
 
-
 # load libraries
 # =============================================================================
 library(ggplot2)
@@ -26,33 +25,37 @@ library(cluster)
 # make sure that your data files are in your current working directory.
 # use getwd() to see your current working directory and setwd() to change it.
 
-# mnist_training <- read.csv("mnist_train.csv") #mnist training data
-# mnist_testing <- read.csv("mnist_test.csv") #mnist testing data
+# here we use a subset of the MNIST dataset (9999 rows of 785 columns)
+# the full data is too large to include for this workshop! It is available
+# online for those interested.
+
+mnist <- read.csv("mnist.csv") #mnist testing data
 data(iris) # iris dataset comes built-in with RStudio
 
 # mnist data set
 # =============================================================================
 
 # set label column as factor for both training and testing
-colnames(mnist_testing)[1] <- "label"
-mnist_testing$label <- factor(as.character(mnist_testing$label))
+colnames(mnist)[1] <- "label"
+mnist$label <- factor(as.character(mnist$label))
 
-colnames(mnist_training)[1] <- "label"
-mnist_training$label <- factor(as.character(mnist_training$label))
 
 # visualize mnist training data with PCA (principal component analysis)
 # =============================================================================
 
-pr <- prcomp(mnist_training[,-1]) # performs PCA
+# if you are interested in learning more about PCA, please talk to us after
+# the workshop ends! For now, consider it a visualization tool for
+# high-dimensional data
+
+pr <- prcomp(mnist[,-1]) # performs PCA
 screeplot(pr, type = "l", main = "mnist training pca screeplot") # screeplot - shows how well
 # the principal components describe the data.
 
 # plot the PCA view of our data (plots two components)
 pca <- autoplot(pr, 
-                data = mnist_training,
+                data = mnist,
                 colour = 'label')
 pca
-
 
 # cluster the mnist data with kmeans
 # =============================================================================
@@ -60,23 +63,22 @@ pca
 t <- Sys.time() # saves current time for timer
 
 set.seed(1)
-km <- kmeans(mnist_testing[,-1], # do not do kmeans on the labels!
+km <- kmeans(mnist[,-1], # do not do kmeans on the labels!
              centers = 10) # we have digits 0 through 9, so 10 clusters
 
 difftime(Sys.time(), t) # calculates the time it took to run this
 
 # confusion matrix: how clean are the clusters?
 table(km$cluster,
-      as.numeric(mnist_testing$label))
+      as.numeric(mnist$label))
 
-accuracy <- sum(km$cluster == as.numeric(mnist_testing$label))/nrow(mnist_testing)
+accuracy <- sum(km$cluster == as.numeric(mnist$label))/nrow(mnist)
+accuracy
 
 # kmeans is not quite the best...why?
 
 # visualization and kmeans with iris dataset
 # =============================================================================
-
-data(iris)
 
 ap_iris <- autoplot(princomp(iris[,-5]),
                     data = iris,
@@ -93,5 +95,4 @@ clustered_iris$cluster = factor(clustered_iris$cluster)
 autoplot(fanny(iris[-5], 3), frame = TRUE)
 
 # accuracy - not too bad!
-#sum(km_iris$cluster == as.numeric(iris$Species))/nrow(iris)
-
+sum(km_iris$cluster == as.numeric(iris$Species))/nrow(iris)
